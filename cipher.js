de9773758f92c03494fcd831f0a7b24d518b3cc7
LYJ <chlrhhan@gmail.com>
2020-08-21 23:22:26 +0900

    crypto 양방향 암호화

--- cipher.js
+++ cipher.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const cipher = cryto.createCipher('aes-256-cbc','열쇠');
+const cipher = crypto.createCipher('aes-256-cbc','열쇠');
 let result = cipher.update('암호화할 문장','utf8','base64');
 result += cipher.final('base64');
 console.log('암호화',result);
