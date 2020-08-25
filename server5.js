4aed3ff272202d2d46ac9cdfd78dac81f41d7045
LYJ <chlrhhan@gmail.com>
2020-08-25 18:53:21 +0900

    쿠키_서버가 사용자 정보 관리하기

--- server5.js
+++ server5.js
@@ -4,11 +4,11 @@ const url = require('url');
 const qs = require('querystring');
 
 const parseCookies = (cookie = '') =>
-    cookie
+    cookie 
         .split(';')
         .map(v => v.split('='))
-        .map(([k, ...vs]) => [k, vs.join('=')])
-        .reduce((acc, [k,v]) => {
+        .map(([k, ...vs])=> [k, vs.join('=')])
+        .reduce((acc,[k,v]) => {
             acc[k.trim()] = decodeURIComponent(v);
             return acc;
         },{});
@@ -17,7 +17,7 @@ const session = {};
 
 http.createServer((req,res) => {
     const cookies = parseCookies(req.headers.cookie);
-    if (req.url.startsWith('/login')){
+    if(req.url.startsWith('/login')){
         const { query } = url.parse(req.url);
         const { name } = qs.parse(query);
         const expires = new Date();
@@ -28,16 +28,15 @@ http.createServer((req,res) => {
             expires,
         };
         res.writeHead(302, {
-            Location: '/',
-            'Set-Cookie': `session=${randomInt}; 
-            Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            Location : '/',
+            'Set-Cookie': `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
     } else if (cookies.session && session[cookies.session].expires > new Date()) {
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
-        res.end('${session[cookies.session].name}님 안녕하세요');
+        res.end(`${session[cookies.session].name}님 안녕하세요`);
     } else {
-        fs.readFile('./server4.html', (err,data) => {
+        fs.readFile('./server4.html',(err,data) => {
             if(err){
                 throw err;
             }
