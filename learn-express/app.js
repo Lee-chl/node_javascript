ffb3fdab981f8f062020e9030cc9ea88fac407c5
LYJ <chlrhhan@gmail.com>
2020-08-31 11:28:43 +0900

    Pug 사용하기전에 템플릿 엔진 pug로 하기

--- learn-express/app.js
+++ learn-express/app.js
@@ -26,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views',path.join(__dirname,'vies'));
+app.set('view engine','pug');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
