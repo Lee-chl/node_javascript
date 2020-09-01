d786ad9c70b859b778ce78f600af342ddd43f911
LYJ <chlrhhan@gmail.com>
2020-09-01 13:12:38 +0900

    view engine를 ejs 바꿔주기

--- learn-express/app.js
+++ learn-express/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views',path.join(__dirname,'vies'));
-app.set('view engine','pug');
+app.set('view engine','ejs');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
