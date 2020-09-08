cd0291bb2017f894ba5b4ef2e1694010bf63cecb
LYJ <chlrhhan@gmail.com>
2020-09-08 15:30:01 +0900

    /profile,/join,/ 페이지

---
+++ nodesnss/routes/page.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile',(req,res) => {
+    res.render('profile',{ title: '내 정보 - Nodesnss',user : null});
+});
+
+router.get('/join',(req,res) => {
+    res.render('join', {
+        title:'회원 가입 - Nodesnss',
+        user: null,
+        joinError: req.flase('joinError'),
+    });
+});
+
+router.get('/',(req,res,next) => {
+    res.render('main', {
+        title: 'NodeSNSs',
+        twits: [],
+        user: null,
+        loginError : req.flash('loginError'),
+    });
+});
+
+module.exports.exports = router;
