ff0992bea0d87e0fe5482a8539feb91f697acb68
LYJ <chlrhhan@gmail.com>
2020-09-10 20:23:42 +0900

    오류 잡기

--- nodesnss/routes/page.js
+++ nodesnss/routes/page.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/profile',(req,res) => {
-    res.render('profile',{ title: '내 정보 - Nodesnss',user : null});
+    res.render('profile', { title: '내 정보 - Nodesnss',user : null});
 });
 
 router.get('/join',(req,res) => {
     res.render('join', {
         title:'회원 가입 - Nodesnss',
         user: null,
-        joinError: req.flase('joinError'),
+        joinError: req.flash('joinError'),
     });
 });
 
@@ -23,4 +23,4 @@ router.get('/',(req,res,next) => {
     });
 });
 
-module.exports.exports = router;
+module.exports = router;
